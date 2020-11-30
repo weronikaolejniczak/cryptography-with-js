da73e9c623dbdd1c2d1041df7200cc7f217ee54e
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-11-30 15:51:13 +0100

    Add scheme for fermat algorithm

---
+++ src/module-1/advanced/fermat.js
@@ -0,0 +1,16 @@
+/**
+ * Algorithm which checks the primality using Fermat's test (missing: 0 pts, implementation up to 5 pts)
+ *
+ * Input: natural number 1 < n, natural number N
+ *
+ * Output: 'yes' if the natural number a was a composite number witness of n; if after N
+ * we have not obtained a composite number certificate, we return 'no conclusion'
+ *
+ * WARNING: choose witness in the range 1 < a < n which are co-prime with n and random.
+ */
+
+function fermat(n, N) {
+    console.log('Algorithm which checks the primality using Fermat\'s test');
+}
+
+module.exports = fermat;
