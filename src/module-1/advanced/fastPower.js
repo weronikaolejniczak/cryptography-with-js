0e1d515bf5fed530489241cec75abec3d9cb2d9c
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-11-30 15:47:48 +0100

    Add scheme for fastPower algorithm

---
+++ src/module-1/advanced/fastPower.js
@@ -0,0 +1,13 @@
+/**
+ * Fast power algorithm (missing: 0 pts, implementation up to 3 pts)
+ *
+ * Input: natural number n > 1, natural number k ≥ 1, natural number 2 ≤ b < n.
+ *
+ * Output: b^k mod n.
+ */
+
+function fastPower(b, k, n) {
+    console.log('Fast power algorithm');
+}
+
+module.exports = fastPower;
