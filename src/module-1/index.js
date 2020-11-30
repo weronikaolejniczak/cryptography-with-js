a36f8cf148b5ec63e1916d20322620c024817dcc
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-11-30 15:58:22 +0100

    Export advanced algorithms from module 1

--- src/module-1/index.js
+++ src/module-1/index.js
@@ -2,8 +2,19 @@ const addBinary = require('./basic/addBinary');
 const multiplyBinary = require('./basic/multiplyBinary');
 const divideBinary = require('./basic/divideBinary');
 
+const findInverse = require('./advanced/findInverse');
+const fastPower = require('./advanced/fastPower');
+const isSquare = require('./advanced/isSquare');
+const squarePrime = require('./advanced/squarePrime');
+const fermat = require('./advanced/fermat');
+
 module.exports = {
     addBinary,
     multiplyBinary,
-    divideBinary
+    divideBinary,
+    findInverse,
+    fastPower,
+    isSquare,
+    squarePrime,
+    fermat
 }
