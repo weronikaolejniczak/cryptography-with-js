dd2be84949a118ee4f31f718afec7841a346d0e3
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-11-30 15:56:39 +0100

    Add scheme for squarePrime algorithm

---
+++ src/module-1/advanced/squarePrime.js
@@ -0,0 +1,17 @@
+/**
+ * Algorithm which finds a square root of a square modulo p (missing: 0 pts, implementation up to 5 pts)
+ *
+ * Input: prime number p congruent to 3 modulo 4, natural number 1 < m < p
+ *
+ * Output:
+ *  if m is not a square modulo p, return 0,
+ *  if m is a square modulo p, return a number 1 < b < p such that p | (b2 − a)
+ *
+ * Hint: apply Euler theorem.
+ */
+
+function squarePrime(p, m) {
+    console.log('Algorithm which finds a square root of a square modulo p');
+}
+
+module.exports = squarePrime;
