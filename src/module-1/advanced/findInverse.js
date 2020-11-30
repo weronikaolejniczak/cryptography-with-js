0576dd0ddf1a67648d9e96ab28a27f812c52c096
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-11-30 15:53:04 +0100

    Add scheme for findInverse algorithm

---
+++ src/module-1/advanced/findInverse.js
@@ -0,0 +1,15 @@
+/**
+ * Finding an inverse modulo n (missing: 0 pts, implementation up to 3 pts)
+ *
+ * Input: natural number n > 1, natural number 0 ≤ a < n
+ *
+ * Output: a message that GCD(a, n) > 1 or a number 0 ≤ b < n such that  n ∣ (ab − 1).
+ *
+ * WARNING: empty list [] on output means that there was no inverse.
+ */
+
+function inverse(a, n) {
+    console.log('Finding an inverse modulo n');
+}
+
+module.exports = inverse;
