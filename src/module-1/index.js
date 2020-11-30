46342fadb16769c9147ab419c799e5a80937ca19
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-11-30 15:45:00 +0100

    Export basic algorithms from module 1

---
+++ src/module-1/index.js
@@ -0,0 +1,9 @@
+const addBinary = require('./basic/addBinary');
+const multiplyBinary = require('./basic/multiplyBinary');
+const divideBinary = require('./basic/divideBinary');
+
+module.exports = {
+    addBinary,
+    multiplyBinary,
+    divideBinary
+}
