af75a6ff107746b484e8cb173de52137749c3b6c
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-11-30 15:38:49 +0100

    Add scheme for divideBinary algorithm

---
+++ src/module-1/basic/divideBinary.js
@@ -0,0 +1,19 @@
+/**
+ * (-1). Division with remainder algorithm (missing: -2 pts, implementation up to 0 pts)
+ *
+ * Input: two binary sequences representing non-negative integers a, b
+ *  a = (ak − 1, …, a0)2,
+ *  b = (bl − 1, …, b0)2
+ * of length k and l respectively, ai, bi ∈ {0, 1}
+ * where a = ∑(k − 1)(i = 0) ai2i, a = ∑(l − 1)(i = 0) bi2i
+ *
+ * Output: two bit sequences q and r satisfying a = q ⋅ b + r and 0 ≤ r < b.
+ *
+ * WARNING: zero is represented by an empty sequence
+ */
+
+function divideBinary(a, b) {
+    console.log('Division with remainder algorithm');
+}
+
+module.exports = divideBinary;
