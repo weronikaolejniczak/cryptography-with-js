6fbea8b6069269c639409bb517c43e7ac7116153
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-11-30 15:39:29 +0100

    Add scheme for multiplyBinary algorithm

---
+++ src/module-1/basic/multiplyBinary.js
@@ -0,0 +1,19 @@
+/**
+ * (-2). School multiplication algorithm (missing: -2 pts, implementation up to 0 pts)
+ *
+ * Input: two binary sequences representing non-negative integers a, b
+ *  a = (ak − 1, …, a0)2,
+ *  b = (bl − 1, …, b0)2
+ * of length k and l respectively, ai, bi ∈ {0, 1}
+ * where
+ *  a = ∑(k − 1)(i = 0) ai2i,
+ *  a = ∑(l − 1)(i = 0) bi2i
+ *
+ * Output: binary sequence c which represents the sum c = a ⋅ b.
+ */
+
+function multiplyBinary(a, b) {
+    console.log('School multiplication algorithm');
+}
+
+module.exports = multiplyBinary;
