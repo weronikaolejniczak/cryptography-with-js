8aff5a534c314178fe46a06f53514bde962063e1
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-12-01 23:37:40 +0100

    Change the called function in entry file

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const moduleOne = require('./src/module-1');
 
-moduleOne.addBinary([1, 0, 0, 1, 0, 0, 1], [1, 0, 1, 0]);
-//moduleOne.multiplyBinary();
+//moduleOne.addBinary([1,1,1,0,1,0,0], [1,1,1,0,1,0,0,0]);
+moduleOne.multiplyBinary([1,1,1,1,0], [0]);
 //moduleOne.divideBinary();
 
 //moduleOne.findInverse();
