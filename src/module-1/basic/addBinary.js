a327ebd718da77273f585e58c8fd508d9f184e38
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-12-01 11:40:14 +0100

    Create the final version of addBinary algorithm

--- src/module-1/basic/addBinary.js
+++ src/module-1/basic/addBinary.js
@@ -20,17 +20,8 @@ function addBinary(a, b) {
     a.reverse();
     b.reverse();
 
-    console.log('A: [', a.toString(), ']');
-    console.log('B: [', b.toString(), ']\n');
-
-    console.log('FIRST PART:');
-
     for (let i = 0; i < Math.min(a.length, b.length); i++) {
-        console.log('at A: ', a[i], ' | ', 'at B: ', b[i]);
-
         if (a[i] == 0 && b[i] == 0) {
-            console.log(i, ': both a and b are 0');
-
             if (carry == 0) {
                 tmp = 0;
             } else {
@@ -38,10 +29,7 @@ function addBinary(a, b) {
             }
 
             carry = 0;
-
         } else if (a[i] == 1 && b[i] == 1) {
-            console.log(i, ': both a and b are 1');
-
             if (carry == 0) {
                 tmp = 0;
             } else {
@@ -49,10 +37,7 @@ function addBinary(a, b) {
             }
 
             carry = 1;
-
         } else {
-            console.log(i, ': either a or b are 1');
-
             if (carry == 0) {
                 tmp = 1;
                 carry = 0;
@@ -63,16 +48,10 @@ function addBinary(a, b) {
         }
 
         c.push(tmp);
-        console.log('tmp: ', tmp, ' | ', 'next carry', carry, '\n');
     }
 
-    console.log('SECOND PART:');
-
     if (a.length > b.length) {
-
         for (let i = b.length; i < a.length; i++) {
-            console.log('at A: ', a[i], ' | ', 'carry: ', carry);
-
             if (a[i] == 0 && carry == 1) {
                 c.push(1);
                 carry = 0;
@@ -83,12 +62,8 @@ function addBinary(a, b) {
                 c.push(a[i]);
             }
         }
-
     } else if (a.length < b.length) {
-
         for (let i = a.length; i < b.length; i++) {
-            console.log('at B: ', b[i], ' | ', 'carry: ', carry);
-
             if (b[i] == 0 && carry == 1) {
                 c.push(1);
                 carry = 0;
@@ -103,7 +78,6 @@ function addBinary(a, b) {
 
     if (carry == 1) c.push(carry);
     c.reverse();
-    console.log('\nRESULT: [', c.toString(), ']');
 
     return c;
 }
