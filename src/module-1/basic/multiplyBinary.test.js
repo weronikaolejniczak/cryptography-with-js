e814b217f985d1f58a34114266d1a8d17f5c81b1
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-12-01 20:51:57 +0100

    Fix tests for multiplyBinary algorithm

---
+++ src/module-1/basic/multiplyBinary.test.js
@@ -0,0 +1,22 @@
+const utilities = require('../utilities');
+const multiplyBinary = require('./multiplyBinary');
+
+const NUM_OF_TESTS = 20;
+
+for (let i = 0; i < NUM_OF_TESTS; i++) {
+    let a = Math.floor(Math.random() * 100);
+    let b = Math.floor(Math.random() * 100);
+
+    let binaryArrayA = utilities.convertToBinary(a);
+    let binaryArrayB = utilities.convertToBinary(b);
+    let binaryArrayProduct = utilities.convertToBinary(a * b);
+
+    test(
+      `expect [${binaryArrayA.toString()}] + [${binaryArrayB.toString()}] to be [${binaryArrayProduct.toString()}]`,
+      () => {
+        let product = multiplyBinary(binaryArrayA, binaryArrayB).toString();
+        let expected = binaryArraySum.toString();
+
+        expect(product).toBe(expected);
+    });
+}
