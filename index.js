d3ed453380fde15bf12087abbb0b922305d8ae83
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-12-01 11:02:40 +0100

    Change entry data for addBinary algorithm

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const moduleOne = require('./src/module-1');
 
-moduleOne.addBinary([1, 0, 0, 1], [1, 1, 1, 0]);
+moduleOne.addBinary([1, 0, 0, 1, 0, 0, 1], [1, 0, 1, 0]);
 //moduleOne.multiplyBinary();
 //moduleOne.divideBinary();
 
