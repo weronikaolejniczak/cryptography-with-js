9f758c0d632096b5abda615966a5594a942ed8bf
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-12-02 16:29:16 +0100

    Change multiplyBinary test scope

--- src/module-1/basic/multiplyBinary.test.js
+++ src/module-1/basic/multiplyBinary.test.js
@@ -4,8 +4,8 @@ const multiplyBinary = require('./multiplyBinary');
 const NUM_OF_TESTS = 20;
 
 for (let i = 0; i < NUM_OF_TESTS; i++) {
-    let a = Math.floor(Math.random() * 50);
-    let b = Math.floor(Math.random() * 50);
+    let a = Math.floor(Math.random() * 100);
+    let b = Math.floor(Math.random() * 100);
 
     let binaryArrayA = utilities.convertToBinary(a);
     let binaryArrayB = utilities.convertToBinary(b);
