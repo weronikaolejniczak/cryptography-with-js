76b7488449a5027ecbd3729231dd10a4a33645b2
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-12-06 14:28:49 +0100

    Add a convertToDecimal utility

--- src/module-1/utilities.js
+++ src/module-1/utilities.js
@@ -2,6 +2,11 @@ function convertToBinary(num) {
     return (num >>> 0).toString(2).split('');
 }
 
+function convertToDecimal(binary) {
+    return parseInt(binary, 2);
+}
+
 module.exports = {
-    convertToBinary
+    convertToBinary,
+    convertToDecimal
 }
