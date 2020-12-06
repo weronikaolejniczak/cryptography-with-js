c0ff3b3cdd4b55e3eaf56c2a4c9f127ca6d67f32
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-12-06 15:57:31 +0100

    Implement findInverse algorithm (doesn't pass all tests)

--- src/module-1/advanced/findInverse.js
+++ src/module-1/advanced/findInverse.js
@@ -3,13 +3,45 @@
  *
  * Input: natural number n > 1, natural number 0 ≤ a < n
  *
- * Output: a message that GCD(a, n) > 1 or a number 0 ≤ b < n such that  n ∣ (ab − 1).
+ * Output:
+ *  a message that GCD(a, n) > 1
+ *  or 
+ *  a number 0 ≤ b < n such that n ∣ (ab − 1).
  *
  * WARNING: empty list [] on output means that there was no inverse.
  */
+const utilities = require('../utilities');
 
-function inverse(a, n) {
-    console.log('Finding an inverse modulo n');
+function recInversion(a, n, b, c) {
+    if (a % n == 0) {
+        b = 0;
+        c = 1;
+
+        return;
+    }
+
+    recInversion(n, a % n, b, c);
+
+    let temp = b;
+
+    b = c;
+    c = temp - c * (a / n);
 }
 
-module.exports = inverse;
+function findInverse(a, n) {
+    let A = utilities.convertToDecimal(a.join(''));
+    let N = utilities.convertToDecimal(n.join(''));
+
+    if (utilities.GCD(A, N) > 1) {
+        return [];
+    }
+
+    let b, c;
+    recInversion(A, N, b, c);
+
+    if (b < 0) b += N;
+
+    return utilities.convertToBinary(b);
+}
+
+module.exports = findInverse;
