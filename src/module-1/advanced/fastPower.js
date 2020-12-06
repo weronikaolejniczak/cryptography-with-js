ad577efa9ebbecf387481590b338b241757007e4
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-12-06 14:10:41 +0100

    Implement fastPower algorithm (doesn't pass all tests)

--- src/module-1/advanced/fastPower.js
+++ src/module-1/advanced/fastPower.js
@@ -5,9 +5,29 @@
  *
  * Output: b^k mod n.
  */
+const utilities = require('../utilities');
 
 function fastPower(b, k, n) {
-    console.log('Fast power algorithm');
+    let result = 1;
+    let base = utilities.convertToDecimal(b.join(''));
+    let power = utilities.convertToDecimal(k.join(''));
+    let mod = utilities.convertToDecimal(n.join(''));
+
+    base = base % mod;
+
+    if (base == 0) return 0;
+
+    while (power > 0) {
+        if (power & 1) {
+            result = (result * base) % mod;
+        }
+        
+        power = power >> 1;
+        base = (base * base) % mod;
+    }
+
+    result = utilities.convertToBinary(result);
+    return result;
 }
 
 module.exports = fastPower;
