2b21711ffd4b78e6cc297558b684c7faca6b6021
Weronika Olejniczak <wer.olejniczak@gmail.com>
2020-12-06 14:37:08 +0100

    Implement isSquare (square modulo p) algorithm

--- src/module-1/advanced/isSquare.js
+++ src/module-1/advanced/isSquare.js
@@ -9,9 +9,20 @@
  *
  * Hint: use Euler theorem to check that.
  */
+const utilities = require('../utilities');
 
 function isSquare(p, m) {
-    console.log('Algorithm which checks whether an element is a square modulo p');
+    p = utilities.convertToDecimal(p.join(''));
+    m = utilities.convertToDecimal(m.join(''));
+    m = m % p;
+
+    for (let i = 2; i < p; i++) {
+        if ((i * i) % p == m) {
+            return 'yes';
+        }
+    }
+
+    return 'no';
 }
 
 module.exports = isSquare;
